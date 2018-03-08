f4861e9cc64388aba7fa1890b6c175b3fb11fdec
Emilie <emilie@emilie-fedora.home>
2018-03-08 12:18:57 +0100

    init files

---
+++ hackmd/docker-entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ "$HMD_DB_URL" = "" ]; then
+    HMD_DB_URL="postgres://hackmd:hackmdpass@hackmdPostgres:5432/hackmd"
+fi
+
+DB_SOCKET=$(echo ${HMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
+
+if [ "$DB_SOCKET" != "" ]; then
+    dockerize -wait tcp://${DB_SOCKET} -timeout 30s
+fi
+
+node_modules/.bin/sequelize db:migrate
+
+# Print warning if local data storage is used but no volume is mounted
+[ "$HMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
+    echo "
+        #################################################################
+        ###                                                           ###
+        ###                        !!!WARNING!!!                      ###
+        ###                                                           ###
+        ###        Using local uploads without persistence is         ###
+        ###            dangerous. You'll loose your data on           ###
+        ###              container removal. Check out:                ###
+        ###  https://docs.docker.com/engine/tutorials/dockervolumes/  ###
+        ###                                                           ###
+        ###                       !!!WARNING!!!                       ###
+        ###                                                           ###
+        #################################################################
+    ";
+} ; }
+
+# Sleep to make sure everything is fine...
+sleep 3
+
+# run
+node app.js
+
